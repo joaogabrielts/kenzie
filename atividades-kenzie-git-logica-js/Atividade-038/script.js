0ca09cd925f54126f9913bbea8fd60d41a0cb5ea
joaogabrielts <teixeirajsilva21@gmail.com>
2024-01-30 15:18:30 -0300

    atualizado atividade

---
+++ atividades-kenzie-git-logica-js/Atividade-038/script.js
@@ -0,0 +1,60 @@
+function renderUpToTwenty(){
+    for(let contagem = 0; contagem <= 20; contagem++){
+        console.log(contagem)
+    }
+
+}
+//renderUpToTwenty()
+
+
+
+function tenlnTenToAHundred(){
+     for(let contagem = 10; contagem <= 100; contagem += 10){
+      console.log(contagem)
+    }
+}
+//tenlnTenToAHundred()
+
+function oddUpToTWenty(){
+    for(let i = 0; i <= 20; i++){
+   if(i % 2 !== 0){
+ console.log(i)
+   }
+    }
+}
+
+//oddUpToTWenty()
+
+function evenUpToTwenty(){
+    for(let i = 0; i <= 20; i++ ){
+   if(i % 2 == 0){
+    console.log(i)
+   }
+    }
+}
+//evenUpToTwenty()
+
+function fromNegativeToPositive(){
+    for(let i = -10; i <= 0 ; i++){
+        console.log(i)
+    }
+   
+}
+//fromNegativeToPositive()
+
+function inDescendingDirection(){
+    for(let i = 10; i >= 0; i-- ){
+        console.log(i)
+    }
+}
+
+//inDescendingDirection()
+
+function toSquare(){
+    for( let i = 1; i <= 10 ; i ++){ 
+        console.log(Math.pow(i,2))
+    }
+}
+
+toSquare()
+
